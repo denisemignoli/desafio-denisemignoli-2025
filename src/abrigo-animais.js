75c793e96e6f3b35ad77c82fb6ff72c94e7fa06d
Denise Mignoli <denisemignoli@gmail.com>
2025-09-08 18:45:02 -0300

    add toy validation

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -8,6 +8,13 @@ class AbrigoAnimais {
     this.animaisMap.set('Bola', { tipo: 'cão', brinquedos: ['CAIXA', 'NOVELO'] });
     this.animaisMap.set('Bebe', { tipo: 'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] });
     this.animaisMap.set('Loco', { tipo: 'jabuti', brinquedos: ['SKATE', 'RATO'] });
+
+    this.brinquedosValidos = new Set();
+    for (const animalInfo of this.animaisMap.values()) {
+      for (const brinquedo of animalInfo.brinquedos) {
+        this.brinquedosValidos.add(brinquedo);
+      }
+    }
   }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
@@ -18,11 +25,28 @@ class AbrigoAnimais {
     if (new Set(ordem).size !== ordem.length) {
       return { erro: 'Animal inválido' };
     }
-
     for (let i = 0; i < ordem.length; i++) {
       const nomeAnimal = ordem[i];
       if (!this.animaisMap.has(nomeAnimal)) {
         return { erro: 'Animal inválido' };
+      }
+    }
+
+    if (new Set(brinquedosP1).size !== brinquedosP1.length) {
+      return { erro: 'Brinquedo inválido' };
+    }
+    for (const brinquedo of brinquedosP1) {
+      if (!this.brinquedosValidos.has(brinquedo)) {
+        return { erro: 'Brinquedo inválido' };
+      }
+    }
+
+    if (new Set(brinquedosP2).size !== brinquedosP2.length) {
+      return { erro: 'Brinquedo inválido' };
+    }
+    for (const brinquedo of brinquedosP2) {
+      if (!this.brinquedosValidos.has(brinquedo)) {
+        return { erro: 'Brinquedo inválido' };
       }
     }
   }
